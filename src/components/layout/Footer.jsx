f1a96f16bae267bfdc6da14ef5aa598c0a5becc9
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-03-08 15:48:22 +0330

    footer fixed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,26 +1,11 @@
-import { Text, Heading, Box } from '@chakra-ui/react';
+import { Text, Heading, Box, Container } from '@chakra-ui/react';
 import React from 'react';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear().toString();
 
   return (
-    <Heading
-      as='h2'
-      textAlign='center'
-      fontSize={15}
-      textTransform='capitalize'
-      bg='whitesmoke'
-      // width={[
-      //   // '15%', // 62em+
-      //   // '25%', // 48em-62em
-      //   // '50%', // 30em-48em
-      //   '100%', // 0-30em
-      // ]}
-      maxW='container.xl'
-      p={0}
-      m={0}
-    >
+    <Container maxW='8xl' textAlign='center'>
       <Text as='p' color='gray.500'>
         parham mosadeqzadeh{' '}
         <Box as='span' color='Highlight'>
@@ -29,7 +14,7 @@ const Footer = () => {
         Front-end developer
       </Text>
       <Text color='gray.400'> &#169; {currentYear}</Text>
-    </Heading>
+    </Container>
   );
 };
 
