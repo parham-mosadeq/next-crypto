40cbba6c62910d9aee134714e4aa1464347c3148
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-03-14 23:41:24 +0330

    exchangeID redesigned

--- src/pages/exchanges/[exchangeID].js
+++ src/pages/exchanges/[exchangeID].js
@@ -1,5 +1,15 @@
 import BackBtn from '@/components/shared/BackBtn';
-import { Box, Container, Grid, GridItem, Image, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Container,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import { fetchExchange } from '@/helpers/api-utils';
 const ExchangeId = ({ exchange }) => {
   const {
@@ -14,91 +24,98 @@ const ExchangeId = ({ exchange }) => {
   return (
     <Container maxW='container.lg' minH='container.lg'>
       <BackBtn />
+      <Card>
+        <Grid
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+          }}
+          gap={3}
+          mt={10}
+          alignContent='center'
+        >
+          <CardHeader>
+            <GridItem
+              width='98'
+              height='56'
+              alignSelf='center'
+              justifySelf={{ base: 'center', md: 'left' }}
+            >
+              <Image src={image} alt={name} width='full' height='full' />
+            </GridItem>
 
-      <Grid
-        templateColumns={{
-          base: 'repeat(1, 1fr)',
-          md: 'repeat(2, 1fr)',
-        }}
-        gap={3}
-        mt={10}
-        alignContent='center'
-      >
-        <GridItem
-          width='28'
-          alignSelf='center'
-          justifySelf={{ base: 'center', md: 'left' }}
-        >
-          <Image src={image} alt={name} width={200} height={120} />
-        </GridItem>
-        <GridItem
-          rounded='3xl'
-          bg='blue.600'
-          width={{ base: 'full' }}
-          mx='auto'
-          textAlign={{ base: 'center', md: 'left' }}
-        >
-          <Box as='div'>
-            <Box position='relative'>
-              <Text
+            <Text
+              color='black'
+              bg={trust_score_rank <= 9 ? 'whatsapp.500' : 'red.500'}
+              width={10}
+              height={10}
+              fontSize={20}
+              mr={4}
+              rounded='full'
+              position='absolute'
+              textAlign='center'
+              top={-4}
+              right={-5}
+            >
+              {trust_score_rank}
+            </Text>
+          </CardHeader>
+
+          <CardBody>
+            <GridItem
+              rounded='3xl'
+              width={{ base: 'full' }}
+              mx='auto'
+              textAlign={{ base: 'center', md: 'left' }}
+            >
+              <Box as='div'>
+                <Box position='relative'>
+                  <Text
+                    as='p'
+                    color='black'
+                    p={2}
+                    textTransform='capitalize'
+                    fontSize={20}
+                  >
+                    name: {name}
+                  </Text>
+                </Box>
+                <Text
+                  as='p'
+                  color='black'
+                  p={2}
+                  textTransform='capitalize'
+                  fontSize={18}
+                >
+                  country:{country}
+                </Text>
+                <Text
+                  as='p'
+                  color='black'
+                  p={2}
+                  textTransform='capitalize'
+                  fontSize={18}
+                >
+                  year established: {year_established} -
+                </Text>
+              </Box>
+
+              <Box
                 as='p'
-                color='white'
+                color='black'
                 p={2}
                 textTransform='capitalize'
-                fontSize={20}
+                fontSize={18}
+                textAlign='left'
               >
-                name: {name}
-              </Text>
-              <Text
-                color='white'
-                bg='whatsapp.600'
-                width={10}
-                height={10}
-                fontSize={20}
-                mr={4}
-                rounded='full'
-                position='absolute'
-                textAlign='center'
-                top={-4}
-                right={0}
-              >
-                {trust_score_rank}
-              </Text>
-            </Box>
-            <Text
-              as='p'
-              color='white'
-              p={2}
-              textTransform='capitalize'
-              fontSize={18}
-            >
-              country:{country}
-            </Text>
-            <Text
-              as='p'
-              color='white'
-              p={2}
-              textTransform='capitalize'
-              fontSize={18}
-            >
-              year established: {year_established} -
-            </Text>
-          </Box>
-
-          <Box
-            as='p'
-            color='white'
-            p={2}
-            textTransform='capitalize'
-            fontSize={18}
-            textAlign='left'
-          >
-            <Text as='a' href={url} target='_blank'>
-              {name} - website
-            </Text>
-          </Box>
-        </GridItem>
-      </Grid>
+                <Text as='a' href={url} target='_blank'>
+                  {name} - website
+                </Text>
+              </Box>
+            </GridItem>
+          </CardBody>
+        </Grid>
+      </Card>
     </Container>
   );
 };
